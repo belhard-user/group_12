24af2b9626029e4cf826eb2f0e6aefc80f011fed
belhard-user <ldarik2l@gmail.com>
2017-04-22 19:51:05 +0300

    test

--- test.php
+++ test.php
@@ -10,16 +10,3 @@ new Base;
 
 new \Foo\Socialite\User;
 new \Foo\Validate\Stringify();
-
-$names = [
-    'Neo',
-    'Morpheus',
-    'Tank',
-    'Trinity',
-    'Dozer',
-    [1, 2, 3, 4, 5, [
-        'asdsa',
-        'sadasd'
-    ]],
-    'Smith'
-];
