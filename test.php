c632c721365336f48a92acd428a231674546d738
belhard-user <ldarik2l@gmail.com>
2017-04-22 18:12:06 +0300

    regexp

--- test.php
+++ test.php
@@ -2,24 +2,35 @@
 
 require_once "vendor/autoload.php";
 
-foo();
-bar();
+function foo($m)
+{
+    dump($m);
+    return "<a href='{$m[1]}'>{$m[2]}</a>";
+}
 
-new User();
-new Base;
+// 1 Search
+// 2 Replace
+// 3 Split
 
-new \Foo\Socialite\User;
-new \Foo\Validate\Stringify();
+$str = 'hello visit my web site http://tut.by http://onliner.by https://ya.ru http://pl.com';
+$re = '#((?:ht|f)tps?://([^.]+)\.\w{2,10})#ui';
+$replacement = '<a href="$1">$2</a>';
 
-$names = [
-    'Neo',
-    'Morpheus',
-    'Tank',
-    'Trinity',
-    'Dozer',
-    [1, 2, 3, 4, 5, [
-        'asdsa',
-        'sadasd'
-    ]],
-    'Smith'
-];
+if (preg_match_all($re, $str, $m, PREG_OFFSET_CAPTURE|PREG_SET_ORDER)) {
+    dump($m);
+}
+
+dump(preg_replace_callback($re, 'foo', [
+    'http://tut.by http://tut.by',
+    'hello http://tut.by',
+    'http://tut.by world',
+    '.!.',
+]));
+
+
+dump(
+    preg_split("/ /", $str)
+);
+
+/*echo preg_replace($re, $replacement, $str, 2, $cnt);
+echo $cnt;*/
